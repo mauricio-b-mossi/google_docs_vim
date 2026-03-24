8b2824321a52f7e57e0f122d1c351df25dba3e12
mauricio mossi <mauricio.b.mossi@gmail.com>
2026-03-23 21:06:08 -0400

    chore: remove onboarding website link

--- src/background.js
+++ src/background.js
@@ -2,9 +2,6 @@
 
 chrome.runtime.onInstalled.addListener((details) => {
     if (details.reason === chrome.runtime.OnInstalledReason.INSTALL) {
-        // Open the external onboarding website when the extension is first installed
-        chrome.tabs.create({
-            url: "https://vimdocs.vercel.app/"
-        });
+        // Onboarding logic can be added here in the future
     }
 });
